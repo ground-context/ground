29345a6e118c4c717bf37cfeefe745aac10f5599
Vikram Sreekanti <viksree@gmail.com>
2016-07-12 14:30:31 -0700

    Modify postgres client to use new function

--- ground-core/src/main/java/edu/berkeley/ground/db/PostgresClient.java
+++ ground-core/src/main/java/edu/berkeley/ground/db/PostgresClient.java
@@ -122,13 +122,16 @@ public class PostgresClient implements DBClient {
 
         public List<String> transitiveClosure(String nodeVersionId) throws GroundException {
             try {
+                /*
                 PreparedStatement statement = this.connection.prepareStatement("with recursive paths(vfrom, vto) as (\n" +
                                                     "    (select endpoint_one, endpoint_two from edgeversions where endpoint_one = ?)\n" +
                                                     "    union\n" +
                                                     "    (select p.vfrom, ev.endpoint_two\n" +
                                                     "    from paths p, edgeversions ev\n" +
                                                     "    where p.vto = ev.endpoint_one)\n" +
-                                                    ") select * from paths;");
+                                                    ") select * from paths;"); */
+
+                PreparedStatement statement = this.connection.prepareStatement("select reachable(?);");
                 statement.setString(1, nodeVersionId);
 
                 ResultSet resultSet = statement.executeQuery();
