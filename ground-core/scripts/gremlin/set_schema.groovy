05080c3d4311a63ee1cd10f4c9a8c0b2d1c3b490
Vikram Sreekanti <viksree@gmail.com>
2016-08-18 20:59:17 +0000

    Update schema set for Gremlin

--- ground-core/scripts/gremlin/set_schema.groovy
+++ ground-core/scripts/gremlin/set_schema.groovy
@@ -1,7 +1,7 @@
-g = TitanFactory.open('/Users/vikram/Code/titan/conf/titan-cassandra.properties')
+g = TitanFactory.open('/home/ubuntu/titan/conf/titan-cassandra.properties')
 management = g.openManagement()
 
-management.makeVertexLabel("GroundEdge").make();
+/*management.makeVertexLabel("GroundEdge").make();
 management.makeVertexLabel("EdgeVersion").make();
 management.makeVertexLabel("Graph").make();
 management.makeVertexLabel("GraphVersion").make();
@@ -22,5 +22,17 @@ management.makeEdgeLabel("RichVersionExternalParameterConnection").multiplicity(
 management.makeEdgeLabel("EdgeVersionConnection").multiplicity(Multiplicity.SIMPLE).make();
 management.makeEdgeLabel("GraphVersionEdge").multiplicity(Multiplicity.ONE2MANY).make();
 management.makeEdgeLabel("LineageEdgeVersionConnection").multiplicity(Multiplicity.SIMPLE).make();
+
+management.makePropertyKey("id").dataType(String.class).make();*/
+management.makePropertyKey("item_id").dataType(String.class).make();
+management.makePropertyKey("node_id").dataType(String.class).make();
+management.makePropertyKey("edge_id").dataType(String.class).make();
+management.makePropertyKey("structure_id").dataType(String.class).make();
+management.makePropertyKey("lineageedge_id").dataType(String.class).make();
+management.makePropertyKey("graph_id").dataType(String.class).make();
+management.makePropertyKey("richversion_id").dataType(String.class).make();
+management.makePropertyKey("tkey").dataType(String.class).make();
+management.makePropertyKey("value").dataType(String.class).make();
+management.makePropertyKey("type").dataType(String.class).make();
 
 management.commit();
