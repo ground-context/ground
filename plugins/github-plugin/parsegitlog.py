cf021c98cac910f065903e965b28923118841234
James Dalton <james.dalton@capitalone.com>
2016-09-14 14:38:43 -0700

    posting to ground
    need a way to look up previous commits in ground. or else manage it in
    the script

--- plugins/github-plugin/parsegitlog.py
+++ plugins/github-plugin/parsegitlog.py
@@ -2,179 +2,118 @@
 
 import json
 import os
+from collections import OrderedDict
 
 import git
 import requests
 from kafka import KafkaConsumer
 import configparser
 
-# TODO make this faster
-# TODO kafka params
+# TODO manage the kafka offset for restarts
 config = configparser.ConfigParser()
 config.read('config.ini')
-consumer = KafkaConsumer(config['Kafka']['topic'], bootstrap_servers=[config['Kafka']['url']+":"+config['Kafka']['port']])
+# consumer = KafkaConsumer('github')
+consumer = KafkaConsumer(config['Kafka']['topic'],
+                         bootstrap_servers=[config['Kafka']['url'] + ":" + config['Kafka']['port']])
+url = 'http://' + config['Ground']['url'] + ':' + config['Ground']['port']
 
 for msg in consumer:
-    # print (msg)
-    parsed_msg = json.loads(msg.value.decode("utf-8"))
 
-    print(msg.key.decode("utf-8"))
-    print(parsed_msg['repository']['git_url'])
+    parsed_msg = json.loads(msg.value.decode("utf-8"))  # turn into json
 
-    # gitUrl = "git@github.com:ground-context/ground.git"
-    gitUrl = parsed_msg['repository']['git_url']  # value from kafka > into json
-    #gitUrl = 'https://github.kdc.capitalone.com/gryzzl/cartographer.git'
-    # repoId = "12341234"
-    repoId = msg.key.decode("utf-8")  # key from kakfa
+    print(msg.key.decode("utf-8"))  # print key
 
-    if not os.path.exists(repoId):
-        # os.makedirs(repoId)
-        # os.chdir(repoId)
-        repo = git.Repo.init(repoId, bare=True)
+    gitUrl = parsed_msg['repository']['git_url']
+    # gitUrl = 'git://github.kdc.capitalone.com/gryzzl/edt.git'
+    print(gitUrl)
+    # repoId = msg.key.decode("utf-8")  # key from kakfa
+    repoId = str(parsed_msg['repository']['id'])
+
+    if not os.path.exists(config['Git']['path'] + repoId):
+        repo = git.Repo.init(config['Git']['path'] + repoId, bare=True)
         origin = repo.create_remote('origin', url=gitUrl)
-        # origin = repo.create_remote('origin', url='git@github.com:ground-context/ground.git')
-        # self("git remote add -f origin "+gitUrl)
     else:
-        # os.chdir(repoId)
-        repo = git.Repo(repoId)
-        # origin = repo.remotes.origin
+        repo = git.Repo(config['Git']['path'] + repoId)
 
-    # TODO get most recent commit from ground, get commits that have happened since
-    repo.remotes.origin.fetch()  # update repo metadata, change to fetch --all, but not important
-    g = git.Git(repoId)
+    repo.remotes.origin.fetch()  # update repo metadata
+    g = git.Git(config['Git']['path'] + repoId)
 
-    l0 = g.log('--all', '--format="%H,%P"')
-    l1 = l0.replace("\"", "").split("\n")
-    l2 = [row.strip().split(",") for row in l1]
-    l2[-1:] = [[l2[-1:][0][0], ""]]
-    l3 = [[row[0], row[1].split()] for row in l2]
+    # URLs
+    nodeUrl = url + '/nodes/' + repoId
+    nodeVUrl = url + '/nodes/versions/'
+    latestUrl = nodeUrl + '/latest'
 
-    r0 = g.log('--all', '--source', '--format=oneline')
-    r1 = r0.split("\n")
-    r2 = [row.split("\t")[1] for row in r1]
-    r3 = [[row.split(" ")[0]] for row in r2]
+    parentsLog = g.log('--all', '--format="%H,%P"')  # log with commit hash and parent hashes
+    parentsLog = parentsLog.replace("\"", "").split("\n")
+    parentsLog = [row.strip().split(",") for row in parentsLog]
+    parentsLog[-1:] = [[parentsLog[-1:][0][0], ""]]  # fix for first commit
+    parentsLog = [[row[0], row[1].split()] for row in parentsLog]
 
-    t = list(zip(r3, l3))
-    t1 = [[row[0][0], row[1][0], row[1][1]] for row in list(t)]
+    sourceLog = g.log('--all', '--source', '--format=oneline')  # log with source branch of commit
+    sourceLog = sourceLog.split("\n")
+    sourceLog = [[row.split("\t")[0], row.split("\t")[1].split(" ")[0]] for row in sourceLog]
+
+    latests = requests.get(latestUrl)  # get latest commits
+    # latests = ['91f8afaae21b6a360ac1e150ddbca342bdb4edfd']
+    for latest in latests:  # remove commits from before the latest commit
+        for item in sourceLog:
+            if latest == item[0]:
+                sourceLog = sourceLog[:sourceLog.index(item)]
+        for item in parentsLog:
+            if latest == item[0]:
+                parentsLog = parentsLog[:parentsLog.index(item)]
+
+    sourceLog = [[row[1]] for row in sourceLog]
     GIT_COMMIT_FIELDS = ['branch', 'commitHash', 'parentHashes']
-    di = [dict(zip(GIT_COMMIT_FIELDS, row)) for row in t1]
+    commitsDict = list(zip(sourceLog, parentsLog))
+    commitsDict = [[row[0][0], row[1][0], row[1][1]] for row in list(commitsDict)]
+    commitsDict = [OrderedDict(zip(GIT_COMMIT_FIELDS, row)) for row in commitsDict]
+    firstCommit = commitsDict[-1:][0]
+    # reorganize dict
+    commitsDict = {a['commitHash']: {'parentHashes': a['parentHashes'], 'branch': a['branch']}
+                   for a in commitsDict}
+    # TODO this needs to be an OrderedDict
 
+    if latests == '[]':
+        requests.post(nodeUrl)  # create initial node in ground
+        nodeVData = {
+            "tags": {
+                "branch": {
+                    "key": "branch",
+                    "value": firstCommit['branch'],
+                    "type": "string"
+                },
+                "commit": {
+                    "key": "commit",
+                    "value": firstCommit['commitHash'],
+                    "type": "string"
+                },
+            },
+            "nodeId": "Nodes." + repoId
+        }
+        r = requests.post(nodeVUrl, data=json.dumps(nodeVData))  # create v1 of node
 
-    for commit in di:
-        print(commit['branch'])
-        # for each commit, create a node version. include the parents of the commit
-        # ?first=1&second=12&third=5
+    for commit in commitsDict:
+        parents = commitsDict[commit]['parentHashes']
+        for parent in parents:
+            parentNode = requests.get(nodeUrl)  # TODO get the parent node id
+            parentNodes = [parentNode if x == parent else x for x in parents]
+        params = {'parents': parentNodes}
 
+        nodeVData = {
+            "tags": {
+                "branch": {
+                    "key": "branch",
+                    "value": commitsDict[commit]['branch'],
+                    "type": "string"
+                },
+                "commit": {
+                    "key": "commit",
+                    "value": commitsDict[commit]['commitHash'],
+                    "type": "string"
+                },
+            },
+            "nodeId": "Nodes." + repoId
+        }
 
-
-
-    lineageName = 'lineageTest'
-
-    nodeUrl = 'localhost:9090/nodes/' + repoId
-    nodeVUrl = 'localhost:9090/nodes/versions/'
-    edgeUrl = 'localhost:9090/edges/' + lineageName
-    edgeVUrl = 'localhost:9090/edges/versions'
-
-    nodeData = {
-        "id": "Nodes." + repoId,
-        "name": repoId
-    }
-
-    nodeVData = {
-        "id": "a0bb1ca6876a6fd69862b9519489b62ccfa7f3f8",
-        "tags": {
-            "testtag": {
-                "versionId": "a0bb1ca6876a6fd69862b9519489b62ccfa7f3f8",
-                "key": "branch",
-                "value": "master",
-                "type": "string"
-            }
-        },
-        "nodeId": "Nodes.ground"
-    }
-
-    edgeData = {
-        "id": "Edges." + repoId,
-        "name": repoId
-    }
-
-    edgeVData = {
-        "id": "abcd",
-        "tags": {
-            "testtag": {
-                "versionId": "abcd",
-                "key": "testtag",
-                "value": "tag",
-                "type": "string"
-            }
-        },
-
-        "reference": "http://www.google.com",
-        "parameters": {
-            "http": "GET"
-        },
-
-        "lineageEdgeId": "LineageEdges.test",
-
-        "fromId": "123",
-
-        "toId": "456"
-    }
-
-    #r = requests.post(url, data=json.dumps(data), )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for each repo ref
-# repo.refs  # gives all the refs
-#
-# for commit in git.objects.commit.Commit.iter_items(repo=r,
-#                                                    rev='refs/remotes/origin/master',
-#                                                    max_count=5):
-#     print(commit)
-#
-# for commit in r.iter_commits('refs/remotes/origin/master', max_count=5):
-#     print("commit: ", commit)
-#     for parent in commit.iter_parents():
-#         print("parent: ", parent)
-
-# git --no-pager log --no-color --all --source --format=oneline
-# 28347a5c3f84b6e5bcff130e4c88b60f1f1a7713  refs/remotes/origin/master Add README
-#
-# git rev-list --remotes --parents --all
-# 9b08df6f57603e57f562427b6ecb870ae04256a9 96d4c4b88d7c10df373024beb8e4a7ca237c32f8 7c68804f0875d97b7b44d4ccb554e9362ffb9eb4
-# Pretend as if all the refs in refs/ are listed on the command line as <commit>.
-# git rev-list --all #lists all commits
-
-# r.git.log()
-# needs to be on a branch
-# checkout master?
-# start at very first commit
-# find all commits that dont have children, get all the parents of all the commits
-
-# g = git.Git("/Users/gfz081/Desktop/bare-repo")
-# g.log('--all', max_parents=0) #get the very first commit
-# g.log('--all', '--branches=*') #get all commits
-
-# counting commits
-# sum(1 for _ in repo.iter_commits())
-# O(n)
-
-# lists all commits
-# git rev-list --remotes
-
-# https://docs.python.org/3/library/subprocess.html#subprocess.run
-# subprocess.run
-
-# print(json.dumps(log, indent=4))
+        requests.post(nodeVUrl, params=params, data=json.dumps(nodeVData))
