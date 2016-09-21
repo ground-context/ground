fec3f5f10b384a9435130df5879ad5fe2a51b4d2
James Dalton <james.dalton@capitalone.com>
2016-09-21 13:12:01 -0700

    apache license

--- ground-core/src/main/java/edu/berkeley/ground/util/KafkaProduce.java
+++ ground-core/src/main/java/edu/berkeley/ground/util/KafkaProduce.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.berkeley.ground.util;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -23,7 +37,6 @@ public class KafkaProduce {
         props.put("acks", "all");
         props.put("retries", 0);
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        //TODO serialize as json not as byte[]
         props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
 
         Producer<String, byte[]> producer = new KafkaProducer<>(props);
