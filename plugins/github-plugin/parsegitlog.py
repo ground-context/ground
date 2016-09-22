8c01eb85c2da2b6d29277dfd11e4983cc0dc0a94
James Dalton <james.dalton@capitalone.com>
2016-09-22 16:23:43 -0700

    refactor git parse
    can fail if a commit has a parent not listed in 'latest' api call

--- plugins/github-plugin/parsegitlog.py
+++ plugins/github-plugin/parsegitlog.py
@@ -35,48 +35,88 @@ import configparser
 # TODO manage the kafka offset for restarts, error handling
 
 
-def get_commits(git_repo):
+def get_commits(git_repo, latest_hashes):
     parents_log = git_repo.log('--all', '--format="%H,%P"')  # log with commit hash and parent hashes
     parents_log = parents_log.replace("\"", "").split("\n")
     parents_log = [row.strip().split(",") for row in parents_log]
     parents_log[-1:] = [[parents_log[-1:][0][0], ""]]  # fix for first commit
     parents_log = [[row[0], row[1].split()] for row in parents_log]
-
     source_log = git_repo.log('--all', '--source', '--format=oneline')  # log with source branch of commit
     source_log = source_log.split("\n")
     source_log = [[row.split("\t")[0], row.split("\t")[1].split(" ")[0]] for row in source_log]
+
+    # TODO can probably do this with less looping
+    for c_hash, node in latest_hashes.items():  # remove commits from before the latest commit
+        for item in source_log:
+            if c_hash == item[0]:
+                source_log = source_log[:source_log.index(item)]
+        for item in parents_log:
+            if c_hash == item[0]:
+                parents_log = parents_log[:parents_log.index(item)]
 
     source_log = [[row[1]] for row in source_log]
     fields = ['branch', 'commitHash', 'parentHashes']
     commits_dict = list(zip(source_log, parents_log))
     commits_dict = [[row[0][0], row[1][0], row[1][1]] for row in list(commits_dict)]
-    commits_dict = [OrderedDict(zip(fields, row)) for row in commits_dict]
-    firstCommit = commits_dict[-1:][0]
-    i = len(commits_dict)
-    for commit in commits_dict:
-        commit.update({'num': i})
-        i -= 1
-    commits_dict = {a['commitHash']: {'parentHashes': a['parentHashes'],
-                    'branch': a['branch'], 'num': int(a['num'])} for a in
-                    commits_dict}
+    commits_dict = [dict(zip(fields, row)) for row in commits_dict]
     return commits_dict
 
 
-def get_new_commits(commits):
-    latests = requests.get(latestUrl).text  # get latest commits # TODO get the latest commit
-
-    # TODO do this with less looping. could just POST until we get to the latest node
-    for latest in latests:  # can there be multiple latest versions?
-        for item in commits:  # loop through all commits until you get to the latest one store in ground
-            if latest == item:
-                commits = commits[:commits.index(item)]  # remove everything before the latest commit
-
-
-
-
+def latest_commits(repo_id):  # TODO fails if a commit has a parent that is not in 'latest' list
+    latests_url = url + '/nodes/Nodes.' + repo_id + '/latest'
+    node_url = url + '/nodes/versions/'
+    node_ids = requests.get(latests_url).json()  # send the request
+    #print('nodeids ' + str(node_ids))
+    hashes = {}
+    for node_id in node_ids:
+        commit_hash = requests.get(node_url+node_id).json()['tags']['commit']['value']
+        hashes[commit_hash] = node_id
+    return hashes
 
 
+def post_commits(commits):
 
+    node_ids = latest_commits(repoId)
+    print('nodeIDs ' + str(node_ids))
+    for c in reversed(commits):  # start at the first commmit
+        params = {}
+        parents = c['parentHashes']  # what are the parent hashes
+        if len(parents) != 0:  # check if there are parents to this commit
+            #print("parents " + str(parents))
+            parent_nodes = []
+            for parent in parents:
+                #parent_node = node_ids[parent]  # get the node id for this this hash
+                parent_nodes.append(node_ids[parent])
+                #parent_nodes = [parent_node if x == parent else x for x in parents]  # add to parentNodes array
+            params = {'parents': parent_nodes}  # create params for the request
+        node_version_data = {
+            "tags": {
+                "branch": {
+                    "key": "branch",
+                    "value": c['branch'],
+                    "type": "string"
+                },
+                "commit": {
+                    "key": "commit",
+                    "value": c['commitHash'],
+                    "type": "string"
+                },
+            },
+            "structureVersionId": None,
+            "reference": None,
+            "nodeId": "Nodes." + repoId,
+            "parameters": {}
+        }
+        headers = {
+            'content-type': "application/json"
+        }
+        r = requests.post(nodeVUrl, params=params, data=json.dumps(node_version_data), headers=headers)  # send the request
+        print(json.dumps(node_version_data))
+        #print(params)
+        print(r.url)
+        #print(r)
+        #print(r.json())
+        node_ids[c['commitHash']] = r.json()['id']  # this will map ground's node id with the commit hash
 
 
 config = configparser.ConfigParser()
@@ -89,14 +129,10 @@ nodeVUrl = url + '/nodes/versions/'
 for msg in consumer:
 
     parsed_msg = json.loads(msg.value.decode("utf-8"))  # turn into json
-
-    print("Message key/Repo name: "+msg.key.decode("utf-8"))  # print key
-
+    print("Received repo: "+msg.key.decode("utf-8"))  # print key
     gitUrl = parsed_msg['repository']['git_url']
-    # gitUrl = 'git://github.kdc.capitalone.com/gryzzl/edt.git'
-    print("Git URL: "+gitUrl)
     # repoId = msg.key.decode("utf-8")  # key from kakfa
-    repoId = str(parsed_msg['repository']['id'])
+    repoId = str(parsed_msg['repository']['id'])  # TODO should repo id be the name instead
     # create URLS for API interaction
     nodeUrl = url + '/nodes/' + repoId
     latestUrl = nodeUrl + '/latest'
@@ -110,73 +146,10 @@ for msg in consumer:
     repo.remotes.origin.fetch()  # update repo metadata
     g = git.Git(config['Git']['path'] + repoId)
 
-    commits = get_commits(g)
+    latests = latest_commits(repoId)
+    if not bool(latests):
+        # create initial node
+        requests.post(nodeUrl)
 
-
-    latest_commits = get_new_commits(commits)
-
-
-
-
-
-    if latests == '[]':  # TODO if this is a new to ground, create a node and a first node version
-        print(latests)
-        requests.post(nodeUrl)  # create initial node in ground
-        nodeVData = {
-            "tags": {
-                "branch": {
-                    "key": "branch",
-                    "value": firstCommit['branch'],
-                    "type": "string"
-                },
-                "commit": {
-                    "key": "commit",
-                    "value": firstCommit['commitHash'],
-                    "type": "string"
-                },
-            },
-            "structureVersionId": None,
-            "reference": None,
-            "nodeId": "Nodes." + repoId,
-            "parameters": {}
-        }
-
-        headers = {
-            'content-type': "application/json"
-        }
-        r = requests.post(nodeVUrl, data=json.dumps(nodeVData), headers=headers)
-        nodeId = r.json()['id']
-        print(nodeId)
-        commits_dict[firstCommit['commitHash']].update({'nodeId': nodeId})
-
-    for commit in commits_dict:
-        parents = commits_dict[commit]['parentHashes']
-        parentNodes = []
-        for parent in parents:
-            print(commits_dict[parent])
-            parentNode = commits_dict[parent]['nodeId']
-            parentNodes = [parentNode if x == parent else x for x in parents]
-        params = {'parents': parentNodes}
-
-        nodeVData = {
-            "tags": {
-                "branch": {
-                    "key": "branch",
-                    "value": commits_dict[commit]['branch'],
-                    "type": "string"
-                },
-                "commit": {
-                    "key": "commit",
-                    "value": commit,
-                    "type": "string"
-                },
-            },
-            "nodeId": "Nodes." + repoId
-        }
-        headers = {
-            'content-type': "application/json"
-        }
-        r = requests.post(nodeVUrl, params=params, data=json.dumps(nodeVData), headers=headers)
-        print(commit)
-        nodeId = r.json()['id']
-        commits_dict[commit].update({'nodeId': nodeId})
+    repo_commits = get_commits(g, latests)  # Get a list of the latest commits
+    post_commits(repo_commits)
