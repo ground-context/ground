ee0f89e92ee91c7b32d70f2fab4e77a53053d6e2
Vikram Sreekanti <viksree@gmail.com>
2017-02-08 15:26:59 -0800

    Change IdGenerator to generate longs instead of Strings

--- ground-core/scripts/postgres/postgres.sql
+++ ground-core/scripts/postgres/postgres.sql
@@ -1,13 +1,13 @@
 /* VERSIONS */
 
 create table version (
-    id varchar NOT NULL PRIMARY KEY
+    id bigint NOT NULL PRIMARY KEY
 );
 
 create table version_successor (
-    id varchar NOT NULL PRIMARY KEY,
-    from_version_id varchar NOT NULL REFERENCES version(id),
-    to_version_id varchar NOT NULL REFERENCES version(id),
+    id bigint NOT NULL PRIMARY KEY,
+    from_version_id bigint NOT NULL REFERENCES version(id),
+    to_version_id bigint NOT NULL REFERENCES version(id),
     CONSTRAINT version_successor_unique_endpoints UNIQUE (from_version_id, to_version_id)
 );
 
@@ -17,7 +17,7 @@ create table item (
 
 create table version_history_dag (
     item_id varchar NOT NULL REFERENCES item(id),
-    version_successor_id varchar NOT NULL REFERENCES version_successor(id),
+    version_successor_id bigint NOT NULL REFERENCES version_successor(id),
     CONSTRAINT version_history_dag_pkey PRIMARY KEY (item_id, version_successor_id)
 );
 
@@ -32,32 +32,32 @@ create table structure (
 );
 
 create table structure_version (
-    id varchar NOT NULL PRIMARY KEY REFERENCES version(id),
+    id bigint NOT NULL PRIMARY KEY REFERENCES version(id),
     structure_id varchar NOT NULL REFERENCES structure(item_id)
 );
 
 create table structure_version_attribute (
-    structure_version_id varchar NOT NULL REFERENCES structure_version(id),
+    structure_version_id bigint NOT NULL REFERENCES structure_version(id),
     key varchar NOT NULL,
     type varchar NOT NULL,
     CONSTRAINT structure_version_attribute_pkey PRIMARY KEY(structure_version_id, key)
 );
 
 create table rich_version (
-    id varchar NOT NULL PRIMARY KEY REFERENCES version(id),
-    structure_version_id varchar REFERENCES structure_version(id),
+    id bigint NOT NULL PRIMARY KEY REFERENCES version(id),
+    structure_version_id bigint REFERENCES structure_version(id),
     reference varchar
 );
 
 create table rich_version_external_parameter (
-    rich_version_id varchar NOT NULL REFERENCES rich_version(id),
+    rich_version_id bigint NOT NULL REFERENCES rich_version(id),
     key varchar NOT NULL,
     value varchar NOT NULL,
     CONSTRAINT rich_version_external_parameter_pkey PRIMARY KEY (rich_version_id, key)
 );
 
 create table tag (
-    rich_version_id varchar REFERENCES rich_version(id),
+    rich_version_id bigint REFERENCES rich_version(id),
     key varchar NOT NULL,
     value varchar,
     type data_type,
@@ -80,25 +80,25 @@ create table graph (
 );
 
 create table node_version (
-    id varchar NOT NULL PRIMARY KEY REFERENCES rich_version(id),
+    id bigint NOT NULL PRIMARY KEY REFERENCES rich_version(id),
     node_id varchar NOT NULL REFERENCES node(item_id)
 );
 
 create table edge_version (
-    id varchar NOT NULL PRIMARY KEY REFERENCES rich_version(id),
+    id bigint NOT NULL PRIMARY KEY REFERENCES rich_version(id),
     edge_id varchar NOT NULL REFERENCES edge(item_id),
     from_node_version_id varchar NOT NULL REFERENCES node_version(id),
     to_node_version_id varchar NOT NULL REFERENCES node_version(id)
 );
 
 create table graph_version (
-    id varchar NOT NULL PRIMARY KEY REFERENCES rich_version(id),
+    id bigint NOT NULL PRIMARY KEY REFERENCES rich_version(id),
     graph_id varchar NOT NULL REFERENCES graph(item_id)
 );
 
 create table graph_version_edge (
-    graph_version_id varchar NOT NULL REFERENCES graph_version(id),
-    edge_version_id varchar NOT NULL REFERENCES edge_version(id),
+    graph_version_id bigint NOT NULL REFERENCES graph_version(id),
+    edge_version_id bigint NOT NULL REFERENCES edge_version(id),
     CONSTRAINT graph_version_edge_pkey PRIMARY KEY (graph_version_id, edge_version_id)
 );
 
@@ -120,10 +120,10 @@ create table lineage_edge (
 );
 
 create table lineage_edge_version (
-    id varchar NOT NULL PRIMARY KEY REFERENCES rich_version(id),
+    id bigint NOT NULL PRIMARY KEY REFERENCES rich_version(id),
     lineage_edge_id varchar NOT NULL REFERENCES lineage_edge(item_id),
-    from_rich_version_id varchar NOT NULL REFERENCES rich_version(id),
-    to_rich_version_id varchar NOT NULL REFERENCES rich_version(id),
+    from_rich_version_id bigint NOT NULL REFERENCES rich_version(id),
+    to_rich_version_id bigint NOT NULL REFERENCES rich_version(id),
     workflow_id varchar REFERENCES graph_version(id),
     principal_id varchar REFERENCES node_version(id)
 );
--- ground-core/src/main/java/edu/berkeley/ground/util/IdGenerator.java
+++ ground-core/src/main/java/edu/berkeley/ground/util/IdGenerator.java
@@ -14,37 +14,25 @@
 
 package edu.berkeley.ground.util;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-
 public class IdGenerator {
-  private static int counter = 0;
+  private long prefix;
+  private long counter;
 
-  private static int numBytes = 20;
-  private static final String SEED = byteArrayToString(new SecureRandom().generateSeed(numBytes));
+  public IdGenerator(int machineId, int numMachines) {
+    long machineBits = 1;
+    long fence = 2;
 
-  public static String generateId(String baseId) {
-
-    return SHA1(SEED + counter++ + baseId);
-  }
-
-  private static String SHA1(String str) {
-    try {
-      MessageDigest md = MessageDigest.getInstance("SHA1");
-      return byteArrayToString(md.digest(str.getBytes()));
-    } catch (NoSuchAlgorithmException e) {
-      throw new RuntimeException("FATAL ERROR: No SHA1 algorithm found in MessageDigest.");
-    }
-  }
-
-  private static String byteArrayToString(byte[] array) {
-    StringBuilder sb = new StringBuilder();
-
-    for (byte b : array) {
-      sb.append(Integer.toString(b & 0xff + 0x100, 16).substring(1));
+    while (fence < numMachines) {
+      fence = fence * 2;
+      machineBits++;
     }
 
-    return sb.toString();
+    this.prefix = machineId << (64 - machineBits);
+
+    this.counter = 0;
+  }
+
+  public synchronized long generateId() {
+    return prefix | counter++;
   }
 }
