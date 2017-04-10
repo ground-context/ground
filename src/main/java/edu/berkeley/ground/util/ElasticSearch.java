b15022844a2625609f695246f1bb799f41cce3b2
Nipun Ramakrishnan <nipun.ramk@berkeley.edu>
2017-04-10 03:05:32 -0700

    Fixed Mapper Parsing Errors

--- src/main/java/edu/berkeley/ground/util/ElasticSearch.java
+++ src/main/java/edu/berkeley/ground/util/ElasticSearch.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.berkeley.ground.exceptions.GroundException;
 import edu.berkeley.ground.model.models.Tag;
-import edu.berkeley.ground.model.versions.GroundType;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.node.Node;
@@ -23,12 +22,11 @@ import java.util.List;
 public class ElasticSearch {
   private static Node node;
   private static Client client;
-  private static String clusterName = "groundtest";
+  private static String clusterName = "groundv1";
 
 
 
   public static boolean connectElasticSearch() throws GroundException {
-    System.out.println("CONNECTING TO ELASTIC SEARCH");
     try {
       node = nodeBuilder().clusterName(clusterName).node();
       client = node.client();
@@ -62,10 +60,6 @@ public class ElasticSearch {
   }
 
   public static List<Long> getSearchResponse(String type, String searchQuery) throws GroundException {
-
-
-    SearchResponse test = client.prepareSearch().get();
-    System.out.println(test.toString());
     SearchResponse response = client.prepareSearch().setTypes(type).setQuery(QueryBuilders.matchQuery("key", searchQuery)).get();
     SearchHit[] hits = response.getHits().getHits();
 
@@ -92,10 +86,13 @@ public class ElasticSearch {
   }
 
   public static void closeElasticSearch() {
-    System.out.println("CLOSING ELASTIC SEARCH!");
     node.close();
     client.close();
   }
+
+
+
+
 
 
 
