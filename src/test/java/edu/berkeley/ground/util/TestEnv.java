a2ca9397928a9286a80d2f0989e0938d02788ccd
Gerard Maas <gerard.maas@gmail.com>
2017-04-15 18:33:17 +0200

    adds config support to tests

---
+++ src/test/java/edu/berkeley/ground/util/TestEnv.java
@@ -0,0 +1,17 @@
+package edu.berkeley.ground.util;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+public class TestEnv {
+
+  private static final String TEST_CONFIG = "test.conf";
+
+  public static final Config config = ConfigFactory.load(TEST_CONFIG);
+
+  private TestEnv() {
+    // NOOP Constructor - static class
+    System.out.println(config);
+  }
+
+}
