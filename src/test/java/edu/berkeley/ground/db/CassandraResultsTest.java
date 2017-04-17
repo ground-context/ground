50eae05ae8ed07cbbd6f691423b8a53efbe5d04e
Gerard Maas <gerard.maas@gmail.com>
2017-04-17 11:24:03 +0200

    adds coverage to CassandraResults; removes few unused imports

---
+++ src/test/java/edu/berkeley/ground/db/CassandraResultsTest.java
@@ -0,0 +1,122 @@
+package edu.berkeley.ground.db;
+
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import edu.berkeley.ground.exceptions.GroundDbException;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.mockito.Mockito.*;
+
+public class CassandraResultsTest {
+  @Test
+  public void shouldGetStringFromValidResultSet() throws GroundDbException {
+    ResultSet rs = mock(ResultSet.class);
+    Row row = mock(Row.class);
+    when(row.getString("field")).thenReturn("value");
+    when(rs.one()).thenReturn(row);
+    CassandraResults results = new CassandraResults(rs);
+    assertThat(results.getString("field")).isEqualTo("value");
+  }
+
+  @Test(expected = GroundDbException.class)
+  public void shouldFailWhenFieldInvalid() throws GroundDbException {
+    setupInvalidField("field").getString("field");
+  }
+
+  @Test
+  public void shouldGetBooleanFromValidResultSet() throws GroundDbException {
+    ResultSet rs = mock(ResultSet.class);
+    Row row = mock(Row.class);
+    when(row.getString("trueField")).thenReturn("true");
+    when(row.getString("falseField")).thenReturn("false");
+    when(row.getString("trueCapitalizedField")).thenReturn("True");
+    when(row.getString("falseCapitalField")).thenReturn("FALSE");
+    when(rs.one()).thenReturn(row);
+    CassandraResults results = new CassandraResults(rs);
+    assertThat(results.getBoolean("trueField")).isEqualTo(true);
+    assertThat(results.getBoolean("falseField")).isEqualTo(false);
+    assertThat(results.getBoolean("trueCapitalizedField")).isEqualTo(true);
+    assertThat(results.getBoolean("falseCapitalField")).isEqualTo(false);
+  }
+
+  // Should this be expected to fail? - it does not!
+//  @Test(expected = GroundDbException.class)
+//  public void shouldFailFromInvalidValue() throws GroundDbException {
+//    ResultSet rs = mock(ResultSet.class);
+//    Row row = mock(Row.class);
+//    when(row.getString("newsField")).thenReturn("alternativeFalse");
+//    when(rs.one()).thenReturn(row);
+//    CassandraResults results = new CassandraResults(rs);
+//    results.getBoolean("newsField");
+//  }
+
+  @Test
+  public void shouldGetIntegerFromValidResultSet() throws GroundDbException {
+    ResultSet rs = mock(ResultSet.class);
+    Row row = mock(Row.class);
+    when(row.getString("field")).thenReturn("42");
+    when(rs.one()).thenReturn(row);
+    CassandraResults results = new CassandraResults(rs);
+    assertThat(results.getInt("field")).isEqualTo(42);
+  }
+
+  @Test
+  public void shouldGetLongFromValidResultSet() throws GroundDbException {
+    ResultSet rs = mock(ResultSet.class);
+    Row row = mock(Row.class);
+    when(row.getLong("field")).thenReturn(120L);
+    when(rs.one()).thenReturn(row);
+    CassandraResults results = new CassandraResults(rs);
+    assertThat(results.getLong("field")).isEqualTo(120L);
+  }
+  
+  @Test(expected = GroundDbException.class)
+  public void shouldFailGetIntegerFromInvalidField() throws GroundDbException {
+    setupInvalidField("field").getInt("field");
+  }
+
+  @Test(expected = GroundDbException.class)
+  public void shouldFailGetIntegerFromInvalidContent() throws GroundDbException {
+    ResultSet rs = mock(ResultSet.class);
+    Row row = mock(Row.class);
+    when(row.getString("field")).thenReturn("not-int");
+    when(rs.one()).thenReturn(row);
+    new CassandraResults(rs).getInt("field");
+  }
+
+  @Test
+  public void shouldMoveToNextRecord() throws GroundDbException {
+    ResultSet rs = mock(ResultSet.class);
+    Row row1 = mock(Row.class);
+    Row row2 = mock(Row.class);
+    when(row2.getString("field")).thenReturn("2");
+    when(rs.one()).thenReturn(row1).thenReturn(row2);
+    CassandraResults results = new CassandraResults(rs);
+    assertThat(results.next()).isTrue();
+    assertThat(results.getInt("field")).isEqualTo(2);
+  }
+
+  @Test
+  public void shouldMoveUpToLastRecord() throws GroundDbException {
+    ResultSet rs = mock(ResultSet.class);
+    Row row1 = mock(Row.class);
+    Row row2 = mock(Row.class);
+    when(rs.one()).thenReturn(row1).thenReturn(row2).thenReturn(null);
+    CassandraResults results = new CassandraResults(rs);
+    assertThat(results.next()).isTrue();
+    assertThat(results.next()).isFalse();
+  }
+
+
+  private CassandraResults setupInvalidField(String fieldName) {
+    ResultSet rs = mock(ResultSet.class);
+    Row row = mock(Row.class);
+    when(row.getString(fieldName)).thenThrow(new IllegalArgumentException("invalid field"));
+    when(rs.one()).thenReturn(row);
+    return new CassandraResults(rs);
+  }
+
+
+
+}
