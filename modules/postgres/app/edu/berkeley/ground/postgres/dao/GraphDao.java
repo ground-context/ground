4fcd9c9a7ff80bdf35638fe7642edb83150e962e
James Dalton <tarpdalton@gmail.com>
2017-04-26 15:03:23 -0700

    google code style changes

--- modules/postgres/app/edu/berkeley/ground/postgres/dao/GraphDao.java
+++ modules/postgres/app/edu/berkeley/ground/postgres/dao/GraphDao.java
@@ -1,19 +1,20 @@
 package edu.berkeley.ground.postgres.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import edu.berkeley.ground.lib.exception.GroundException;
 import edu.berkeley.ground.lib.model.core.Graph;
 import edu.berkeley.ground.postgres.utils.PostgresUtils;
+import java.util.ArrayList;
+import java.util.List;
 import play.db.Database;
 
 public class GraphDao {
 
-    public final void create(final Database dbSource, final Graph graph) throws GroundException {
-        final List<String> sqlList = new ArrayList<>();
-        sqlList.add(String.format("insert into graph (item_id, source_key, name) values (%d,%s, %s)", graph.getId(),
-                graph.getSourceKey(), graph.getName()));
-        PostgresUtils.executeSqlList(dbSource, sqlList);
-    }
+  public final void create(final Database dbSource, final Graph graph) throws GroundException {
+    final List<String> sqlList = new ArrayList<>();
+    sqlList.add(
+        String.format(
+            "insert into graph (item_id, source_key, name) values (%d,%s, %s)",
+            graph.getId(), graph.getSourceKey(), graph.getName()));
+    PostgresUtils.executeSqlList(dbSource, sqlList);
+  }
 }
