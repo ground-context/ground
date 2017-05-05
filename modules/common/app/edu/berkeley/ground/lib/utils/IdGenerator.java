a61456ae4c6df11126e5c4e0baef0226272eb52c
Eric Liu <ericsliu@gmail.com>
2017-05-05 13:31:49 -0700

    deleted 'lib' from gitignore, changed IdGenerator location

--- modules/common/app/edu/berkeley/ground/lib/utils/IdGenerator.java
+++ modules/common/app/edu/berkeley/ground/lib/utils/IdGenerator.java
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package edu.berkeley.ground.lib.utils;
 
 import javax.inject.Singleton;
