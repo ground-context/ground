4af3420a34e4de2a3d81f94fb7703fe5ae8bf3c7
Vikram Sreekanti <vsreekanti@gmail.com>
2017-05-21 22:23:41 -0700

    Add sbt-dist back for travis

--- resources/ci/init.sh
+++ resources/ci/init.sh
@@ -8,3 +8,5 @@ sudo service postgresql restart
 # create Postgres db and user
 sudo su -c "createdb test" -s /bin/sh postgres
 sudo su -c "createuser test -d -s" -s /bin/sh postgres
+
+cd resources/scripts/postgres && python2.7 postgres_setup.py test test && cd ../../..
